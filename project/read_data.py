43710a5f8e2252d4fd8322d5d7c5c5ec6cd6605a
Raghav Rathi <raghav17083@iiitd.ac.in>
2021-04-12 19:16:17 +0530

    paper-citation matrix

--- project/read_data.py
+++ project/read_data.py
@@ -5,8 +5,10 @@ Created on Mon Apr 12 11:37:02 2021
 @author: Raghav Rathi
 """
 
-paper_id_dic={}
-paper_title_dic={}
+import numpy as np
+
+global paper_id_dic={}
+global paper_title_dic={}
 
 with open("datasets_inUse/paper_ids.txt","r") as file:
     pid=0
@@ -19,7 +21,30 @@ with open("datasets_inUse/paper_ids.txt","r") as file:
         pid+=1
         
         
-        
-print(paper_id_dic)
+# print(paper_id_dic)
+
+nop=len(paper_id_dic)
+
+
     
-    
+#%%
+
+""""Paper Citation matrix"""
+# import platform
+# print(platform.architecture())
+
+def paper_citation_matrix(path):
+    with open(path,'r') as file:
+        matrix=np.zeros((nop,nop))
+        for i in file.readlines():
+            l=i.split()
+            matrix[paper_id_dic[l[0]],paper_id_dic[l[2]]]=1
+# print(matrix)
+
+"""Checking sparsity. Takes very long"""
+cnt=0
+for i in range(nop):
+    for j in range(nop):
+        if(matrix[i,j]!=0):
+            cnt+=1
+print(cnt)
